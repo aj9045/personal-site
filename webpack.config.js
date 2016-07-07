ad84ef3923eb6b7d23a70d3d9ccb4f0b43295426
aj9045 <identity_unveiled@hotmail.com>
2016-07-07 15:53:01 -0400

    add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: [
+    "./src/index.js"
+  ],
+  output: {
+    path: __dirname,
+    publicPath: "/",
+    fileName: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: "babel"
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  }
+};
