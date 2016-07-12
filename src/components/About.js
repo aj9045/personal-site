fb8581020f736f60d5f3d2f9e8ef25a359cf94bd
aj9045 <identity_unveiled@hotmail.com>
2016-07-12 03:23:06 -0400

    change class name

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import React, { Component } from "react";
 export default class About extends Component {
   render() {
     return (
-      <div className="contact">
+      <div className="about">
         About stuff here
       </div>
     );
