2efaf1c8ce16e4f79f35be59d0c70b453cfaaecc
aj9045 <identity_unveiled@hotmail.com>
2016-07-12 03:20:46 -0400

    add onclick event and value to change tabs

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,9 +5,9 @@ export default class Nav extends Component {
   render() {
     return (
       <div className="nav">
-        <div className="nav-about">About</div>
-        <div className="nav-projects">Projects</div>
-        <div className="nav-contact">Contact</div>
+        <div className="nav-about" onClick={this.props.changeTab} value="about">About</div>
+        <div className="nav-projects" onClick={this.props.changeTab} value="projects">Projects</div>
+        <div className="nav-contact"onClick={this.props.changeTab} value="contact">Contact</div>
       </div>
     );
   }
