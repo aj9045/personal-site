91c5b4fc46d5c528ed1ebb64bbdbfe112b72fef6
aj9045 <identity_unveiled@hotmail.com>
2016-07-12 03:21:47 -0400

    aimport modules and add prop and func to change selected tab state

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Nav from "./components/Nav.js";
+import About from "./components/About.js";
+import Projects from "./components/Projects.js";
+import Contact from "./components/Contact.js";
 
 class App extends Component {
+  changeTab(e) {
+    var selected = e.target.getAttribute("value");
+
+    this.setState({selected: selected});
+  }
+
   render() {
     return (
       <div className="appContent">
-        <Nav />
+        <Nav changeTab={this.changeTab.bind(this)} />
         <div className="appContent-welcome">
           Welcome!
         </div>
