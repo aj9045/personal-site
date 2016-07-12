f05cdf7888117668b9bdb248e49bc8867f2fecbb
aj9045 <identity_unveiled@hotmail.com>
2016-07-12 03:20:24 -0400

    add simple about component

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+
+export default class About extends Component {
+  render() {
+    return (
+      <div className="contact">
+        About stuff here
+      </div>
+    );
+  }
+}
