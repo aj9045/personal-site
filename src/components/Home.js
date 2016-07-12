bae6968b6475d4dc8dabe69ebaaaf1310df8d4bd
aj9045 <identity_unveiled@hotmail.com>
2016-07-12 11:10:35 -0400

    add simple home component

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        About stuff here
+      </div>
+    );
+  }
+}
