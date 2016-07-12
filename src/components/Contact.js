611d6b573c82a110ee707bcec03d7a056f660e8d
aj9045 <identity_unveiled@hotmail.com>
2016-07-12 11:11:29 -0400

    add unordered list or contact/social media icons and links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,28 @@ export default class Contact extends Component {
     return (
       <div className="contact">
         contact info here
+        <ul className="contact-list">
+          <li className="contact-list-item">
+            <a href="mailto:stuhrenberg.aj@gmail.com" className="contact-link"><img src={"../../images/email.png"} alt="email icon" className="contact-email icon" />stuhrenberg.aj@gmail.com
+            </a>
+          </li>
+          <li className="contact-list-item">
+            <a href="http://linkedin.com/in/ajstuhrenberg" className="contact-link"><img src={"../../images/linkedin.png"} alt="linkedin icon" className="contact-linkedin icon" />/ajstuhrenberg
+            </a>
+          </li>
+          <li className="contact-list-item">
+            <a href="http://github.com/aj9045" className="contact-link"><img src={"../../images/github.png"} alt="github icon" className="contact-github icon" />aj9045
+            </a>
+          </li>
+          <li className="contact-list-item">
+            <a href="http://behance.net/aj9045" className="contact-link"><img src={"../../images/behance.png"} alt="behance icon" className="contact-behance icon" />aj9045
+            </a>
+          </li>
+          <li className="contact-list-item">
+            <a href="http://instagram.com/ajs9045" className="contact-link"><img src={"../../images/instagram.png"} alt="instagram icon" className="contact-instagram icon" />@ajs9045
+            </a>
+          </li>
+        </ul>
       </div>
     );
   }
