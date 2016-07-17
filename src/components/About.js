4b4a70fe660c5f3e9623385c27ff9d81fdfc0ab5
aj9045 <identity_unveiled@hotmail.com>
2016-07-17 06:31:56 -0400

    add about me paragraph

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,9 @@ export default class About extends Component {
   render() {
     return (
       <div className="about">
-        About stuff here
+        <p className="about-me">
+        Hey all! My name is AJ Stuhrenberg. I recently graduated from Dev Bootcamp, an 18 week web development intensive learning experience, in December of 2014, where I learned a good amount of Ruby, Rails, JavaScript, HTML5, and CSS3. And most recently, I worked at Texts.com, a small startup in NYC. I developed the first versions of their iOS and Android apps using React Native. And I am always looking to learn more! Right now, I’m brushing up on JavaScript, ReactJS, and learning Redux. Please feel free to contact me with questions, comments, or anything else.
+        </p>
       </div>
     );
   }
