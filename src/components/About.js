0d5bea124fa2a4b0dc96283dd35ebc7a8c48983d
aj9045 <identity_unveiled@hotmail.com>
2016-07-17 09:44:21 -0400

    edit about me and move button up with a greeting at the top

--- src/components/About.js
+++ src/components/About.js
@@ -25,10 +25,12 @@ export default class About extends Component {
 
     return (
       <div className="about">
-        <p className="about-me">
-        Hey all! My name is AJ Stuhrenberg. I recently graduated from Dev Bootcamp, an 18 week web development intensive learning experience, in December of 2014, where I learned a good amount of Ruby, Rails, JavaScript, HTML5, and CSS3. And most recently, I worked at Texts.com, a small startup in NYC. I developed the first versions of their iOS and Android apps using React Native. And I am always looking to learn more! Right now, I’m brushing up on JavaScript, ReactJS, and learning Redux. Please feel free to contact me with questions, comments, or anything else.
-        </p>
         <button type="button" onClick={this.props.openModal} className="showResume btn">Resume</button>
+        <h1 className="about-greeting">Hey all!</h1>
+        <p className="about-me">
+        My name is AJ Stuhrenberg. Most recently, I worked at Texts.com, a small startup in NYC. I developed the first versions of their iOS and Android apps using React Native. I graduated from Dev Bootcamp, an 18 week web development intensive learning experience, in December of 2014, where I learned a good amount of Ruby, Rails, JavaScript, HTML5, and CSS3. And I am always looking to learn more! Right now, I’m brushing up on JavaScript, ReactJS, and learning Redux.
+        </p>
+
         {modal}
       </div>
     );
