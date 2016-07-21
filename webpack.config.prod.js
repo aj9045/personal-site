658767f804810923d471aa284f1cdc9dd2230632
aj9045 <identity_unveiled@hotmail.com>
2016-07-21 14:38:47 -0400

    add webpack production config

---
+++ webpack.config.prod.js
@@ -0,0 +1,35 @@
+module.exports = {
+  entry: [
+    "./src/index.js"
+  ],
+  output: {
+    path: __dirname,
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: "babel"
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env":{
+        "NODE_ENV":
+  JSON.stringify("production")
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    }),
+    new webpack.optimize.AggressiveMergingPlugin()
+  ]
+};
