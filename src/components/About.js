a53613784e3b6f79cf834625728904d19f4e46bc
aj9045 <identity_unveiled@hotmail.com>
2016-07-21 14:03:59 -0400

    fix indentation

--- src/components/About.js
+++ src/components/About.js
@@ -28,9 +28,9 @@ export default class About extends Component {
         <button type="button" onClick={this.props.openModal} className="showResumeButton btn">Resume</button>
         <h1 className="about-greeting">Hey all!</h1>
         <p className="about-me">
-        <img src="../../images/me.jpg" className="about-photo" />
+          <img src="../../images/me.jpg" className="about-photo" />
 
-        My name is AJ Stuhrenberg. Most recently, I worked at Texts.com, a small startup in NYC. I developed the first versions of their iOS and Android apps using React Native. I graduated from Dev Bootcamp, an 18 week web development intensive learning experience, in December of 2014, where I learned a good amount of Ruby, Rails, JavaScript, HTML5, and CSS3. And I am always looking to learn more! Right now, I’m brushing up on JavaScript, ReactJS, and learning Redux.
+          My name is AJ Stuhrenberg. Most recently, I worked at Texts.com, a small startup in NYC. I developed the first versions of their iOS and Android apps using React Native. I graduated from Dev Bootcamp, an 18 week web development intensive learning experience, in December of 2014, where I learned a good amount of Ruby, Rails, JavaScript, HTML5, and CSS3. And I am always looking to learn more! Right now, I’m brushing up on JavaScript, ReactJS, and learning Redux.
         </p>
 
         {modal}
